f457caa9ba9b5eba0fe769be0744bbceff1d0d87
christopher bolocan <chris.bolocan@gmail.com>
2026-02-04 16:35:07 +0100

    Arreglo fallo raro

--- gestion_peluquerias/__manifest__.py
+++ gestion_peluquerias/__manifest__.py
@@ -4,6 +4,9 @@
     'depends': ['base'],
     'data': [
         'security/ir.model.access.csv',
+        'views/stylehub_clientes_view.xml',
+        'views/stylehub_estilistas_view.xml',
+        'views/stylehub_menu_view.xml',
         'views/stylehub_servicios_view.xml',
     ],
     'installable': True,
